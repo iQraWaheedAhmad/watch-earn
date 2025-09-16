7d5727d42f99d3e6b4aea7944dbf14bbf85151ae
iqra <igrawaheed213@gmail.com>
2025-09-16 16:34:22 +0500

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${geistMono.variable} font-sans antialiased`}>
         <AuthProvider>
-        
+        <Navbar />
           <main className="pt-16">{children}</main>
           
         </AuthProvider>
